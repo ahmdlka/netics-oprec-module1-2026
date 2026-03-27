a7612d657474bd0cb991abac47c1241e35b76ebb
kaka <lokaarziki@gmail.com>
2026-03-27 20:09:55 +0700

    selesai kayaknya

--- src/main.go
+++ src/main.go
@@ -21,8 +21,8 @@ func getHealth(c *gin.Context) {
 	c.JSON(
 		http.StatusOK,
 		gin.H{
-			"nama":      "Sersan Mirai Afrizal",
-			"nrp":       "5025241999",
+			"nama":      "Ahmad Loka Arziki",
+			"nrp":       "5025241044",
 			"status":    "UP",
 			"timestamp": time.Now().Format(time.RFC3339),
 			"uptime":    time.Since(timeStart).String(),
